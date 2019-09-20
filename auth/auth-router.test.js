4f67cfb8fce6c12a225f63c06af71195213cff85
Bilguun Tse <bbg.1015@gmail.com>
2019-09-20 13:51:35 -0500

    added some test for post requests

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,18 +1,31 @@
-// const request = require('supertest');
-// const router = require('./auth-router.js');
-// const Users = require('../users/users-model.js')
+const request = require('supertest');
+const router = require('./auth-router.js');
+const Users = require('../users/users-model.js')
 
-// describe('auth-router.js', () => {
-//     describe('POST /register', () => {
-//         it('returns saved user', () => {
-//             return request(router)
-//                 .post('/register')
-//                 .then(res => {
-//                     Users.add
-//                         .then(res => {
-//                             expect(res.status).toBe(200)
-//                         })
-//                 })
-//         })
-//     })
-// })
+describe('auth-router.js', () => {
+    describe('POST /register', () => {
+        it('returns saved user', () => {
+                request(router)
+                .post('/register')
+                .send('username=Joel','password=pass')
+                .set('Accept', 'application/json')
+                .expect(201)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    done(); })
+        });
+    });
+    describe('POST /login', () => {
+        it('returns a cookie', () => {
+                request(router)
+                .post('/login')
+                .send('username=Joel','password=pass')
+                .set('token', 'application/json')
+                .expect(201)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    done(); })
+        });
+    });
+   
+})
